85213cc15049569d4f96be0283ca1867218b14da
Mark Arciaga <mark.arciaga@gmail.com>
2018-05-14 09:19:18 -0700

    update to use snipcart live API key

--- components/layout.js
+++ components/layout.js
@@ -17,7 +17,7 @@ export const Layout = ({ children }) => (
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-            <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MDczMjUxOTEtOTI0Yy00MWExLWE2NjYtOTBhNzc4NzE4OTUyNjM2NTYzMjA1NzE2NDk1MDQ0" id="snipcart"></script>
+            <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MmNlZmFiMmUtZWNiYS00OTUzLWI3YTAtYzQ0NWZkMzkwNmRlNjM2NTYzMjA1NzE2NDk1MDQ0" id="snipcart"></script>
             <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
             <link rel='stylesheet' href='/_next/static/style.css' />
         </Head>
